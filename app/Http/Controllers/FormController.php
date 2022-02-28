f730c6ca9ca72b8a2088b0c2283e43e5a5d5900c
HaniifAJ <haniif02aj@gmail.com>
2022-02-28 20:03:26 +0700

    first upload

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,35 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+ 
+class FormController extends Controller
+{
+    public function formulir()
+    {
+        return view('formulir');
+    }
+ 
+    public function proses(Request $request)
+    {
+        $messagesError = [
+            'required' => ':attribute ini wajib diisi!!',
+            'min' => ':attribute harus diisi minimal :min karakter!!!',
+            'max' => ':attribute harus diisi maksimal :max karakter!!!',
+            'numeric' => ':attribute harus berisi angka'
+        ];
+        
+        $this->validate($request,[
+            'nama' => 'required|min:5|max:20',
+            'nrp' => 'required|numeric',
+            'jurusan' => 'required',
+            'usia' => 'required|numeric',
+            'hobby' => 'required',
+            'foto' => 'required|image|mimes:jpeg,png,jpg'
+        ],$messagesError);
+
+        return view('proses',['data' => $request]);
+    }
+}
